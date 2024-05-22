4457302f864c6f52ddb64b0c4986963e94037bba
DennisvEngelen <344709@student.fontys.nl>
2024-05-22 11:23:28 +0200

    initial commit

---
+++ src/test/java/IndividueelDVE/followservice/FollowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package IndividueelDVE.followservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
